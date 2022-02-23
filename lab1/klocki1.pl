e7b9ac6755527ac894da30180f2fdfdb68736132
Daniel Kucharski <danku@onet.eu>
2022-02-23 13:52:38 +0100

    commit

---
+++ lab1/klocki1.pl
@@ -0,0 +1,14 @@
+% PROGRAM: klocki_1
+% Baza wiedzy o klackach
+% Definiowane predykaty:
+% na/2
+% =============================
+%
+% na(X,Y)
+% opis: spe³niony, gdy klocek X le¿y
+% bezpoœrednio na klocku Y
+% =============================
+na(c,a).
+na(c,b).
+na(d,c).
+
